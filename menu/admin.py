362887007fe4ed33ca5430bb5ae624bb287f7975
Akila <akila271819@gmail.com>
2025-10-07 23:56:39 +0530

    init

--- menu/admin.py
+++ menu/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Category, Item
 
 admin.site.register(Category)
 admin.site.register(Item)
-=======
-
-# Register your models here.
->>>>>>> e15e063d4c1e5d4c2805f61d97193ed308e6a6e9
